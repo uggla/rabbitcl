0203f25a2627e7dbcf71f88e921d1a4fa09acd84
Uggla <uggla@free.fr>
2019-06-04 08:34:13 +0000

    Change run.sh

--- node2/run.sh
+++ node2/run.sh
@@ -3,18 +3,23 @@
 set -euo pipefail
 IFS=$'\n\t'
 
+wait_rabbit() {
+	while ! curl -s localhost:15672 | grep "title" | grep "RabbitMQ Management"
+	do
+		sleep 5s
+	done
+}
+
 if [[ -f data ]]; then
 	rm -rf data logs
 fi
+
 mkdir -p data logs
 chown -R 999 data logs
 cp .erlang.cookie data
 docker-compose up -d
 
-while ! curl -s localhost:15672 | grep "title" | grep "RabbitMQ Management"
-do
-	sleep 5s
-done
+wait_rabbit
 docker exec rabbit rabbitmqctl stop_app
 sleep 2s
 docker exec rabbit rabbitmqctl join_cluster rabbit@node-1.uggla.fr
--- node3/run.sh
+++ node3/run.sh
@@ -3,10 +3,28 @@
 set -euo pipefail
 IFS=$'\n\t'
 
+wait_rabbit() {
+	while ! curl -s localhost:15672 | grep "title" | grep "RabbitMQ Management"
+	do
+		sleep 5s
+	done
+}
+
 if [[ -f data ]]; then
 	rm -rf data logs
 fi
+
 mkdir -p data logs
 chown -R 999 data logs
 cp .erlang.cookie data
 docker-compose up -d
+
+wait_rabbit
+docker exec rabbit rabbitmqctl stop_app
+sleep 2s
+docker exec rabbit rabbitmqctl join_cluster rabbit@node-1.uggla.fr
+docker exec rabbit rabbitmqctl start_app
+
+wait_rabbit
+# Set queues in HA
+docker exec rabbit rabbitmqctl set_policy ha "." '{"ha-mode":"all"}'
