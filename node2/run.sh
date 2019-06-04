61b4df022a8539807a2277b177b97715b0b3ce57
Uggla <uggla@free.fr>
2019-06-04 08:26:06 +0000

    Update compose and run.sh files

--- node2/run.sh
+++ node2/run.sh
@@ -10,3 +10,12 @@ mkdir -p data logs
 chown -R 999 data logs
 cp .erlang.cookie data
 docker-compose up -d
+
+while ! curl -s localhost:15672 | grep "title" | grep "RabbitMQ Management"
+do
+	sleep 5s
+done
+docker exec rabbit rabbitmqctl stop_app
+sleep 2s
+docker exec rabbit rabbitmqctl join_cluster rabbit@node-1.uggla.fr
+docker exec rabbit rabbitmqctl start_app
